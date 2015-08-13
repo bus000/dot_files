97381a5f707c1d2429679483faee09226fa9c9e3
Bus000 <magnus@stavngaard.dk>
2015-08-14 01:00:23 +0200

    Make statusbar appear on top of windows
    To make the status bar not just go to the background every time an
    application is started.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,20 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad defaultConfig
-    { terminal    = "xterm"
-    , borderWidth = 3
-    }
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+
+    xmonad $ defaultConfig
+        { terminal    = "xterm"
+        , borderWidth = 3
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        }
