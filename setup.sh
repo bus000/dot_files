891f65f763fb3417a5dec9fda2030c9404e5d881
Bus000 <magnus@stavngaard.dk>
2015-08-13 23:46:06 +0200

    Change file names to non hidden
    As the dot files are kept in another directory and only symbolically
    linked to the home directory, there is no need to keep them as hidden
    files. In fact, it is much easier to keep track of them when they show
    up on a normal ls.

--- setup.sh
+++ setup.sh
@@ -3,25 +3,25 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 rm ~/.bashrc 2> /dev/null
-ln -s $DIR/.bashrc ~/.bashrc
+ln -s $DIR/bashrc ~/.bashrc
 
 rm ~/.inputrc 2> /dev/null
-ln -s $DIR/.inputrc ~/.inputrc
+ln -s $DIR/inputrc ~/.inputrc
 
 rm ~/.tmux.conf 2> /dev/null
-ln -s $DIR/.tmux.conf ~/.tmux.conf
+ln -s $DIR/tmux.conf ~/.tmux.conf
 
 rm ~/.Xdefaults 2> /dev/null
-ln -s $DIR/.Xdefaults ~/.Xdefaults
+ln -s $DIR/Xdefaults ~/.Xdefaults
 
 rm -R ~/.xmonad 2> /dev/null
-ln -s $DIR/.xmonad ~/.xmonad
+ln -s $DIR/xmonad ~/.xmonad
 
 rm ~/.vimrc 2> /dev/null
-ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/vimrc ~/.vimrc
 
 rm ~/.xinitrc 2> /dev/null
-ln -s $DIR/.xinitrc ~/.xinitrc
+ln -s $DIR/xinitrc ~/.xinitrc
 
 rm ~/.xsessionrc 2> /dev/null
-ln -s $DIR/.xsessionrc ~/.xsessionrc
+ln -s $DIR/xsessionrc ~/.xsessionrc
