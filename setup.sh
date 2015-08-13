7f584724994a88d4ae5a385d7733ad4ddb00b73d
Bus000 <magnus@stavngaard.dk>
2015-08-13 09:48:24 +0200

    Add script to easy setup on new computer
    The script set up a symbolic link from all dot files in the git
    directory to the users home directory allowing them to be used.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+rm ~/.bashrc 2> /dev/null
+ln -s $DIR/.bashrc ~/.bashrc
+
+rm ~/.inputrc 2> /dev/null
+ln -s $DIR/.inputrc ~/.inputrc
+
+rm ~/.tmux.conf 2> /dev/null
+ln -s $DIR/.tmux.conf ~/.tmux.conf
+
+rm ~/.Xdefaults 2> /dev/null
+ln -s $DIR/.Xdefaults ~/.Xdefaults
+
+rm -R ~/.xmonad 2> /dev/null
+ln -s $DIR/.xmonad ~/.xmonad
+
+rm ~/.vimrc 2> /dev/null
+ln -s $DIR/.vimrc ~/.vimrc
+
+rm ~/.xinitrc 2> /dev/null
+ln -s $DIR/.xinitrc ~/.xinitrc
+
+rm ~/.xsessionrc 2> /dev/null
+ln -s $DIR/.xsessionrc ~/.xsessionrc
