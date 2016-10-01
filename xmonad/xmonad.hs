3bb02a6e6bebdf85ad054f9e437d17558dd2b476
Bus000 <magnus@stavngaard.dk>
2016-10-01 22:27:05 +0200

    Change colors and text

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,27 +1,30 @@
+import qualified Data.Map as Map
+import Data.Monoid
+import System.IO
 import XMonad
+import XMonad.Actions.Volume
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
-import XMonad.Actions.Volume
-import Data.Map
-import Data.Monoid
+import XMonad.Util.Run(spawnPipe)
 
 main = do
     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
 
-    xmonad $ defaultConfig
+    xmonad $ def
         { terminal     = "xterm"
         , borderWidth  = 3
-        , manageHook   = manageDocks <+> manageHook defaultConfig
-        , layoutHook   = avoidStruts  $  layoutHook defaultConfig
+        , normalBorderColor = "#222222"
+        , focusedBorderColor = "#222222"
+        , manageHook   = manageDocks <+> manageHook def
+        , layoutHook   = avoidStruts  $  layoutHook def
+        , workspaces = ["www", "code"] ++ map show [3..9]
         , logHook      = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle  = xmobarColor "green" "" . shorten 50
             }
-        , XMonad.keys = XMonad.keys defaultConfig `mappend`
-            \c -> fromList [ ((0, xK_F6), toggleMute >> return ())
+        , XMonad.keys = XMonad.keys def `mappend`
+            \c -> Map.fromList [ ((0, xK_F6), toggleMute >> return ())
                            , ((0, xK_F7), setMute False >> lowerVolume 4 >> return ())
                            , ((0, xK_F8), setMute False >> raiseVolume 4 >> return ())
                            ]
